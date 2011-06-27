6baa9d9bd435111064d30d1594c116170512b04a
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2011-06-28 00:06:33 +0300

    Fabfile updated.
    Update Fabric fabfile for remote deployment on the Maemo device. New
    option is to update project files directly (put) withput rsync.

--- fabfile.py
+++ fabfile.py
@@ -1,22 +1,31 @@
+from __future__ import with_statement
 import os
+from fabric.api import env, put, run, cd
+from fabric.contrib.project import rsync_project, upload_project
 
-from fabric.api import env, put, run
-from fabric.contrib.project import rsync_project
-
-env.user = 'root'
+env.user = 'user'
 env.hosts = ['192.168.2.15']
 
 TARGET_WORKSPACE = '/home/user/CodeVault/'
-TARGET_PROJECT = 'maebird'
-TARGET_MAIN_FILE = 'maebird/main.py'
+TARGET_PROJECT = os.path.join(TARGET_WORKSPACE, 'maebird')
+TARGET_MAIN_FILE = 'main.py'
 TARGET = os.path.join(TARGET_WORKSPACE, TARGET_PROJECT, TARGET_MAIN_FILE)
 
-def sync_n_run():
-    sync(TARGET_WORKSPACE)
-    run_remote(TARGET)
+EXCLUDES = ['*.pyc', '*.pyo']
+
+def deploy_n_run(remote='sync'):
+    if remote == 'sync':
+        sync()
+    elif remote == 'upload':
+        upload()
+    run_remote(TARGET_PROJECT, TARGET_MAIN_FILE)
     
-def sync(path):
-    rsync_project(path)
+def sync(remotedir=TARGET_WORKSPACE, localdir=None):
+    rsync_project(remotedir, localdir, exclude=EXCLUDES)
     
-def run_remote(target, cmd='python'):
-    run('%s %s' % (cmd, target))
+def run_remote(project, mainfile, cmd='python'):
+    with cd(project):
+        run('%s %s' % (cmd, mainfile))
+    
+def upload(localdir=None, remotedir=TARGET_WORKSPACE):
+    upload_project(localdir, remotedir)
